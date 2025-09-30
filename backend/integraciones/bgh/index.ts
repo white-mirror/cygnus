56be6b0e262daf558c26adc5150ac216f771597b
npanaino <npanaino@gmail.com>
2025-09-30 17:14:10 -0300

    chore: update tsconfigs and add bgh integration

---
+++ backend/integraciones/bgh/index.ts
@@ -0,0 +1,14 @@
+﻿export {
+  BGHApiError,
+  BGHAuthenticationError,
+  BGHClient,
+  type BGHClientOptions,
+  type DeviceStatusMap,
+  DeviceStatus,
+  FAN_MODES,
+  HVAC_MODES,
+  DEFAULT_TIMEOUT_MS,
+  API_URL,
+  BASE_URL,
+  LOGIN_ENDPOINT,
+} from "./client";
